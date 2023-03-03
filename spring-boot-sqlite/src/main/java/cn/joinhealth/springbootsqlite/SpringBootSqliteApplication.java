a8b6bc3d6b59ffc3806dd19c343edca39ede226d
jlin <jlin@justhealth.cn>
2023-03-03 13:40:49 +0800

    feat: add spring-boot-sqlite

---
+++ spring-boot-sqlite/src/main/java/cn/joinhealth/springbootsqlite/SpringBootSqliteApplication.java
@@ -0,0 +1,13 @@
+package cn.joinhealth.springbootsqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSqliteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSqliteApplication.class, args);
+    }
+
+}
---
+++ spring-boot-sqlite/src/test/java/cn/joinhealth/springbootsqlite/SpringBootSqliteApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.joinhealth.springbootsqlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSqliteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
